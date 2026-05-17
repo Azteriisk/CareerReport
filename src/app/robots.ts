4d89e2bcb14091cecd3a261a0f9047bc99179acf
Alec Brandt <abrandt0208@gmail.com>
2026-05-17 02:59:31 -0500

    updating seo stuff

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/'],
     },
-    sitemap: 'https://careerreport.com/sitemap.xml',
+    sitemap: 'https://careerreport.azterisk.net/sitemap.xml',
   }
 }
